4f0b9d420e3f5fa4621ac330584e861a5f05f2f9
mBadar-dev <m.badar.dev@gmail.com>
2025-08-08 06:33:50 +0500

    Mega Project with AppWrite

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+  appWriteUrl: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+  appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appWriteProjectName: String(import.meta.env.VITE_APPWRITE_PROJECT_NAME),
+  appWriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+};
+
+export default conf;
